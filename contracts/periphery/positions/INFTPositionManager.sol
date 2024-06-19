a36a32b3a862d9989c5f3b5840eb912e1f9802db
Deadshot Ryker <ryker@zerolend.xyz>
2024-06-20 02:13:53 +0530

    redid proxy contract for nft position manager

--- contracts/periphery/positions/INFTPositionManager.sol
+++ contracts/periphery/positions/INFTPositionManager.sol
@@ -2,6 +2,28 @@
 pragma solidity 0.8.19;
 
 interface INFTPositionManager {
+  /// @notice Error indicating that the caller is not the owner or approved operator of the token ID.
+  error NotTokenIdOwner();
+  /// @notice Error indicating that it is an Zero Address which is not allowed
+  error ZeroAddressNotAllowed();
+  /// @notice Error indicating that value that is passed is zero which is not allowed
+  error ZeroValueNotAllowed();
+
+  /// @notice Error indicating that value that the address is invalid and not present in debt market
+  error InvalidMarketAddress();
+
+  /**
+   * @notice Emitted when an NFT is minted.
+   * @param recipient The address that received the minted NFT.
+   * @param tokenId The ID of the minted NFT.
+   */
+  event NFTMinted(address indexed recipient, uint256 indexed tokenId);
+  /**
+   * @notice Emitted when an NFT is burned.
+   * @param tokenId The ID of the burned NFT.
+   */
+  event NFTBurned(uint256 indexed tokenId);
+
   struct MintParams {
     address market;
     address recipient;
