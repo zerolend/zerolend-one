cab3249b394814fbd1160bd82ed1622cb92575c6
Deadshot Ryker <ryker@zerolend.xyz>
2024-06-22 14:55:43 +0530

    fixed typechain issues

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,10 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox-viem';
+import 'hardhat-deploy';
+import '@typechain/hardhat';
+import '@nomicfoundation/hardhat-ethers';
+import '@nomicfoundation/hardhat-chai-matchers';
+import 'hardhat-abi-exporter';
 import 'hardhat-dependency-compiler';
 import 'solidity-docgen';
 import 'solidity-coverage';
@@ -9,8 +14,17 @@ dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: '0.8.19',
+  abiExporter: {
+    path: './abi',
+    runOnCompile: true,
+    clear: true,
+    spacing: 2,
+  },
   docgen: {
     pages: 'files',
+  },
+  typechain: {
+    outDir: 'types',
   },
 };
 
