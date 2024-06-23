c48f81b4942e89bd818d21f1769ffb687f21d10b
Deadshot Ryker <ryker@zerolend.xyz>
2024-06-24 00:45:01 +0530

    updated script for create2

--- scripts/guess.ts
+++ scripts/guess.ts
@@ -4,9 +4,14 @@ import { getCreate2Address } from './create2';
 
 // declare deployment parameters
 import contractArtifact from '../artifacts/contracts/core/protocol/factory/Factory.sol/Factory.json';
-const constructorTypes = ['address'];
-const constructorArgs = ['0x6aac0942b8147bffab73789a82ee12fda7735bac'];
+const constructorTypes = contractArtifact.abi
+  .find((v) => v.type === 'constructor')
+  ?.inputs.map((t) => t.type);
+
+const constructorArgs: any[] = ['0x6aac0942b8147bffab73789a82ee12fda7735bac'];
 const factoryAddress = '0x6aac0942b8147bffab73789a82ee12fda7735bac';
+
+console.log('constructor parameters', constructorTypes, constructorArgs);
 
 const job = () => {
   let i = 0;
@@ -23,13 +28,13 @@ const job = () => {
       constructorArgs: constructorArgs,
     });
 
-    if (computedAddress.startsWith('0x000000')) {
+    if (computedAddress.startsWith('0x0000000')) {
       console.log('found the right salt hash');
-      console.log('salt', salt);
+      console.log('salt', salt, computedAddress);
       break;
     }
 
-    if (i % 100000 == 0) console.log(i, 'salt', salt);
+    if (i % 100000 == 0) console.log(i, 'salt', salt, computedAddress);
     i++;
   }
 };
