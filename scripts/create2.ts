cfa2671deebce4df52ec7299b35eec9513991a3b
Deadshot Ryker <ryker@zerolend.xyz>
2024-06-24 00:54:41 +0530

    removed code

--- scripts/create2.ts
+++ scripts/create2.ts
@@ -1,13 +1,9 @@
-import { Provider } from '@ethersproject/providers';
 import { ethers, keccak256 } from 'ethers';
 
-export const buildBytecode = (
-  constructorTypes: any[],
-  constructorArgs: any[],
-  contractBytecode: string
-) => `${contractBytecode}${encodeParams(constructorTypes, constructorArgs).slice(2)}`;
+const buildBytecode = (constructorTypes: any[], constructorArgs: any[], contractBytecode: string) =>
+  `${contractBytecode}${encodeParams(constructorTypes, constructorArgs).slice(2)}`;
 
-export const buildCreate2Address = (factoryAddress: string, saltHex: string, byteCode: string) => {
+const buildCreate2Address = (factoryAddress: string, saltHex: string, byteCode: string) => {
   return `0x${keccak256(
     `0x${['ff', factoryAddress, saltHex, keccak256(byteCode)]
       .map((x) => x.replace(/0x/, ''))
@@ -15,26 +11,11 @@ export const buildCreate2Address = (factoryAddress: string, saltHex: string, byt
   ).slice(-40)}`.toLowerCase();
 };
 
-export const numberToUint256 = (value: number) => {
-  const hex = value.toString(16);
-  return `0x${'0'.repeat(64 - hex.length)}${hex}`;
-};
+const saltToHex = (salt: string | number) => ethers.id(salt.toString());
 
-export const saltToHex = (salt: string | number) => ethers.id(salt.toString());
-
-export const encodeParam = (dataType: any, data: any) => {
-  const abiCoder = new ethers.AbiCoder();
-  return abiCoder.encode([dataType], [data]);
-};
-
-export const encodeParams = (dataTypes: any[], data: any[]) => {
+const encodeParams = (dataTypes: any[], data: any[]) => {
   const abiCoder = new ethers.AbiCoder();
   return abiCoder.encode(dataTypes, data);
-};
-
-export const isContract = async (address: string, provider: Provider) => {
-  const code = await provider.getCode(address);
-  return code.slice(2).length > 0;
 };
 
 export function getCreate2Address({
