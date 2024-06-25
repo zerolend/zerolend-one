b7f1953633be3cfda7acd823e605a4013447c445
Deadshot Ryker <ryker@zerolend.xyz>
2024-06-26 02:35:07 +0530

    added the timelock and governance

--- contracts/governance/Governor.sol
+++ contracts/governance/Governor.sol
@@ -13,6 +13,84 @@ pragma solidity 0.8.19;
 // Twitter: https://twitter.com/zerolendxyz
 // Telegram: https://t.me/zerolendxyz
 
-contract Governor {
-  // TODO
+import {GovernorCountingSimple} from '@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol';
+import {GovernorSettings} from '@openzeppelin/contracts/governance/extensions/GovernorSettings.sol';
+import {GovernorTimelockControl} from '@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol';
+import {GovernorVotes} from '@openzeppelin/contracts/governance/extensions/GovernorVotes.sol';
+import {GovernorVotesQuorumFraction} from '@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol';
+import {IGovernor, Governor} from '@openzeppelin/contracts/governance/Governor.sol';
+import {IVotes} from '@openzeppelin/contracts/governance/utils/IVotes.sol';
+import {TimelockController} from '@openzeppelin/contracts/governance/TimelockController.sol';
+
+contract ZeroLendGovernor is
+  Governor,
+  GovernorSettings,
+  GovernorCountingSimple,
+  GovernorVotes,
+  GovernorVotesQuorumFraction,
+  GovernorTimelockControl
+{
+  constructor(
+    IVotes _token,
+    TimelockController _timelock
+  )
+    Governor('ZeroLend Governor')
+    GovernorSettings(7200 /* 1 day */, 50400 /* 1 week */, 10000000e18)
+    GovernorVotes(_token)
+    GovernorVotesQuorumFraction(40)
+    GovernorTimelockControl(_timelock)
+  {}
+
+  function votingDelay() public view override(GovernorSettings, IGovernor) returns (uint256) {
+    return super.votingDelay();
+  }
+
+  function supportsInterface(
+    bytes4 interfaceId
+  ) public view virtual override(Governor, GovernorTimelockControl) returns (bool) {
+    return super.supportsInterface(interfaceId);
+  }
+
+  function votingPeriod() public view override(IGovernor, GovernorSettings) returns (uint256) {
+    return super.votingPeriod();
+  }
+
+  function quorum(
+    uint256 blockNumber
+  ) public view override(IGovernor, GovernorVotesQuorumFraction) returns (uint256) {
+    return super.quorum(blockNumber);
+  }
+
+  function state(
+    uint256 proposalId
+  ) public view override(Governor, GovernorTimelockControl) returns (ProposalState) {
+    return super.state(proposalId);
+  }
+
+  function proposalThreshold() public view override(Governor, GovernorSettings) returns (uint256) {
+    return super.proposalThreshold();
+  }
+
+  function _cancel(
+    address[] memory targets,
+    uint256[] memory values,
+    bytes[] memory calldatas,
+    bytes32 descriptionHash
+  ) internal override(Governor, GovernorTimelockControl) returns (uint256) {
+    return super._cancel(targets, values, calldatas, descriptionHash);
+  }
+
+  function _executor() internal view override(Governor, GovernorTimelockControl) returns (address) {
+    return super._executor();
+  }
+
+  function _execute(
+    uint256 id,
+    address[] memory targets,
+    uint256[] memory values,
+    bytes[] memory calldatas,
+    bytes32 descriptionHash
+  ) internal virtual override(Governor, GovernorTimelockControl) {
+    super._execute(id, targets, values, calldatas, descriptionHash);
+  }
 }
--- contracts/governance/Timelock.sol
+++ contracts/governance/Timelock.sol
@@ -13,6 +13,49 @@ pragma solidity 0.8.19;
 // Twitter: https://twitter.com/zerolendxyz
 // Telegram: https://t.me/zerolendxyz
 
-contract Timelock {
-  // TODO
+import {AccessControl, AccessControlEnumerable} from '@openzeppelin/contracts/access/AccessControlEnumerable.sol';
+import {IERC1155Receiver, TimelockController} from '@openzeppelin/contracts/governance/TimelockController.sol';
+
+contract TimelockControllerEnumerable is TimelockController, AccessControlEnumerable {
+  /**
+   * @dev Sets the value for {name} and {version}
+   */
+  constructor(
+    uint256 minDelay,
+    address[] memory proposers,
+    address[] memory executors,
+    address admin
+  ) TimelockController(minDelay, proposers, executors, admin) {
+    // nothing
+  }
+
+  /**
+   * @dev See {IERC165-supportsInterface}.
+   */
+  function supportsInterface(
+    bytes4 interfaceId
+  ) public view virtual override(TimelockController, AccessControlEnumerable) returns (bool) {
+    return
+      interfaceId == type(IERC1155Receiver).interfaceId || super.supportsInterface(interfaceId);
+  }
+
+  /**
+   * @dev Overload {_grantRole} to track enumerable memberships
+   */
+  function _grantRole(
+    bytes32 role,
+    address account
+  ) internal virtual override(AccessControl, AccessControlEnumerable) {
+    super._grantRole(role, account);
+  }
+
+  /**
+   * @dev Overload {_revokeRole} to track enumerable memberships
+   */
+  function _revokeRole(
+    bytes32 role,
+    address account
+  ) internal virtual override(AccessControl, AccessControlEnumerable) {
+    super._revokeRole(role, account);
+  }
 }
