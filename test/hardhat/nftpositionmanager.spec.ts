691d2204a8d951aa1f28cae243b5ade12d9801a0
SilentHex <shivam@zerolend.xyz>
2024-06-26 20:13:26 +0530

    done-increaseLiquidity-testcases

--- test/hardhat/nftpositionmanager.spec.ts
+++ test/hardhat/nftpositionmanager.spec.ts
@@ -2,10 +2,13 @@ import { ethers, upgrades } from 'hardhat';
 import { deployPool } from './fixtures/pool';
 import { expect } from 'chai';
 import { Contract, Signer, ZeroAddress } from 'ethers';
-import { INFTPositionManager, MintableERC20, NFTPositionManager, Pool } from '../../types';
+import { MintableERC20, NFTPositionManager, Pool } from '../../types';
 
 describe('NFT Position Manager', () => {
-  let positionManager: Contract, factory, pool: Pool, tokenA: MintableERC20;
+  let positionManager: NFTPositionManager;
+  let factory;
+  let pool: Pool;
+  let tokenA: MintableERC20;
   let alice: Signer, bob: Signer;
 
   beforeEach(async () => {
@@ -13,11 +16,16 @@ describe('NFT Position Manager', () => {
     ({ factory, pool, tokenA } = await deployPool());
     // console.log(`Factory Proxy ${await factory.getAddress()}`);
     const NFTPositionManager = await ethers.getContractFactory('NFTPositionManager');
-    positionManager = await upgrades.deployProxy(NFTPositionManager, [await factory.getAddress()], {
-      initializer: 'initialize',
-      kind: 'transparent',
-      unsafeAllow: ['delegatecall'],
-    });
+    const NFTPositionManagerFactory = await ethers.getContractFactory('NFTPositionManager');
+    positionManager = (await upgrades.deployProxy(
+      NFTPositionManagerFactory,
+      [await factory.getAddress()],
+      {
+        initializer: 'initialize',
+        kind: 'transparent',
+        unsafeAllow: ['delegatecall'],
+      }
+    )) as NFTPositionManager;
     await positionManager.waitForDeployment();
     // console.log(`NFT Position Manager Proxy Deployed to ${await positionManager.getAddress()}`);
   });
@@ -86,7 +94,7 @@ describe('NFT Position Manager', () => {
         .withArgs(tokenA.target, 1, supplyAmount);
     });
 
-    it('_handleLiquidity should transfer asset from user to NFT-Position-Manager', async () => {
+    it('_handleLiquidity should transfer asset from user to NFT-Position-Manager & mint NFT', async () => {
       // mint asset tokens to alice
       let mintAmount = ethers.parseUnits('100', 'wei');
       let supplyAmount = ethers.parseUnits('10', 'wei');
@@ -137,6 +145,71 @@ describe('NFT Position Manager', () => {
         'ZeroValueNotAllowed'
       );
     });
-    it('Should revert if the caller is not owner or approved for tokenId', async () => {});
+    it('Should revert if the caller is not owner or approved for tokenId', async () => {
+      let supplyAmount = ethers.parseUnits('10', 'wei');
+      let mintAmount = ethers.parseUnits('100', 'wei');
+      await tokenA.connect(alice)['mint(uint256)'](mintAmount);
+
+      let mintParams = {
+        asset: tokenA,
+        pool,
+        amount: supplyAmount,
+      };
+
+      await tokenA.connect(alice).approve(positionManager.target, supplyAmount);
+
+      await positionManager.connect(alice).mint(mintParams);
+
+      let liquidityParams = {
+        asset: tokenA,
+        pool,
+        amount: supplyAmount,
+        tokenId: 1,
+      };
+      await expect(
+        positionManager.connect(bob).increaseLiquidity(liquidityParams)
+      ).to.be.revertedWithCustomError(positionManager, 'NotTokenIdOwner');
+    });
+    it('_handleLiquidity should transfer asset from user to NFT-Position-Manager', async () => {
+      let mintAmount = ethers.parseUnits('100', 'wei');
+      let supplyAmount = ethers.parseUnits('10', 'wei');
+
+      await tokenA.connect(bob)['mint(uint256)'](mintAmount);
+      expect(await tokenA.balanceOf(await bob.getAddress())).to.be.equals(mintAmount);
+      let mintParams = {
+        asset: tokenA,
+        pool,
+        amount: supplyAmount,
+      };
+      // Approve the NFT Position Manager
+      await tokenA.connect(bob).approve(positionManager.target, supplyAmount);
+      // mint a postion for tokenId 1 by alice
+      await positionManager.connect(bob).mint(mintParams);
+      // Store the positions
+      const position = await positionManager.positions(1);
+      expect(position[0]).to.be.equals(await pool.getAddress());
+      expect(position[1]).to.be.equals(ZeroAddress);
+      expect(await tokenA.balanceOf(pool.target)).to.be.equals(supplyAmount);
+
+      //Bob should be the owner of the nft
+      expect(await positionManager.ownerOf(1)).to.be.equals(await bob.getAddress());
+
+      // Now call the increaseLiquidity and supply the asset to the pool
+
+      await tokenA.connect(bob).approve(positionManager.target, supplyAmount);
+      let liquidityParams = {
+        asset: tokenA,
+        pool,
+        amount: supplyAmount,
+        tokenId: 1,
+      };
+
+      await positionManager.connect(bob).increaseLiquidity(liquidityParams);
+      let balanceAfterLiquidity = await tokenA.balanceOf(pool.target);
+
+      expect(await tokenA.balanceOf(pool.target)).to.be.equals(balanceAfterLiquidity);
+      //Bob still should be the owner of the nft after increase liquidity
+      expect(await positionManager.ownerOf(1)).to.be.equals(await bob.getAddress());
+    });
   });
 });
