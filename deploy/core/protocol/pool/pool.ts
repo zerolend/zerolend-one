52d06aa1526610fa080df893f09740397ae8bbca
nikhilbajaj31 <nikhil@zerolend.xyz>
2024-06-27 12:51:52 +0530

    feat: add pool deployment scripts

---
+++ deploy/core/protocol/pool/pool.ts
@@ -0,0 +1,21 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { getPoolLibraries } from "../../../helpers/contract-getters";
+
+async function main(hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  
+  const deployment = await deploy("Pool", {
+    from: deployer,
+    contract: "Pool",
+    autoMine: true,
+    log: true,
+    libraries: await getPoolLibraries(hre)
+  });
+
+  await hre.run("verify:verify", { address: deployment.address });
+}
+
+main.tags = ["Pool"];
+export default main;
