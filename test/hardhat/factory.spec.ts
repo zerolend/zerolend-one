4924fa97c96eb450fbc44190ca6eee95df204510
Deadshot Ryker <ryker@zerolend.xyz>
2024-06-29 11:51:18 +0530

    fixed factory unit tests

--- test/hardhat/factory.spec.ts
+++ test/hardhat/factory.spec.ts
@@ -1,12 +1,8 @@
-import { ZeroAddress } from 'ethers';
-import {
-  DefaultReserveInterestRateStrategy,
-  IPool,
-  MintableERC20,
-  MockAggregator,
-} from '../../types';
-import { PoolFactory } from '../../types/contracts/core/protocol/pool/PoolFactory';
+import { DataTypes } from '../../types/IPool';
+import { DefaultReserveInterestRateStrategy, MintableERC20, MockAggregator } from '../../types';
 import { deployCore } from './fixtures/core';
+import { PoolFactory } from '../../types/contracts/core/protocol/pool/PoolFactory';
+import { ZeroAddress } from 'ethers';
 
 describe('Factory', () => {
   let poolFactory: PoolFactory;
@@ -27,12 +23,23 @@ describe('Factory', () => {
   });
 
   it('Should create a new pool', async () => {
-    const input: IPool.InitParamsStruct = {
+    const basicConfig: DataTypes.InitReserveConfigStruct = {
+      ltv: 7500,
+      liquidationThreshold: 8000,
+      liquidationBonus: 10500,
+      decimals: 18,
+      frozen: false,
+      borrowable: true,
+      borrowCap: 0,
+      supplyCap: 0,
+    };
+
+    const input: DataTypes.InitPoolParamsStruct = {
       hook: ZeroAddress,
       assets: [tokenA.target, tokenB.target, tokenC.target],
       rateStrategyAddresses: [irStrategy.target, irStrategy.target, irStrategy.target],
       sources: [oracleA.target, oracleB.target, oracleC.target],
-      configurations: [{ data: 0 }, { data: 0 }, { data: 0 }],
+      configurations: [basicConfig, basicConfig, basicConfig],
     };
 
     const tx = await poolFactory.createPool(input);
