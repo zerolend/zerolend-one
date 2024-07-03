df00539f04dc88cf7a4de9df48651989c05c1065
Deadshot Ryker <ryker@zerolend.xyz>
2024-07-03 15:10:40 +0400

    added mroe fn's to the multicall

--- test/hardhat/periphery/nftpositionmanager.multicall.spec.ts
+++ test/hardhat/periphery/nftpositionmanager.multicall.spec.ts
@@ -11,25 +11,36 @@ describe.only('NFT position manager - multicall', () => {
   let pool: Pool;
   let tokenA: MintableERC20;
   let tokenB: MintableERC20;
-  let governance: Signer, alice: Signer, bob: Signer;
+  let governance: Signer, ant: Signer, whale: Signer, owner: Signer;
 
   beforeEach(async () => {
-    [alice, bob] = await ethers.getSigners();
-    ({ poolFactory, pool, tokenA, tokenB, governance } = await deployPool());
+    ({ poolFactory, pool, tokenA, tokenB, governance, ant, whale, owner } = await deployPool());
     manager = await deployNftPositionManager(poolFactory, await governance.getAddress());
 
-    await tokenA.connect(alice)['mint(uint256)'](e18('100'));
-    await tokenA.connect(alice).approve(manager.target, MaxUint256);
+    await tokenA.connect(ant).approve(manager.target, MaxUint256);
+    await tokenA.connect(ant)['mint(uint256)'](e18('100'));
+    await tokenA.connect(whale).approve(manager.target, MaxUint256);
+    await tokenA.connect(whale)['mint(uint256)'](e18('100'));
+    await tokenB.connect(ant).approve(manager.target, MaxUint256);
+    await tokenB.connect(ant)['mint(uint256)'](e18('100'));
 
-    await tokenB.connect(alice)['mint(uint256)'](e18('100'));
-    await tokenB.connect(alice).approve(manager.target, MaxUint256);
+    // seed some liquidity into the pool
+    const mintSupplyCall = await manager.interface.encodeFunctionData('mint', [
+      {
+        asset: tokenA.target,
+        pool: pool.target,
+        amount: e18('100'),
+        data: { interestRateData: '0x', hookData: '0x' },
+      },
+    ]);
+    await manager.connect(whale).multicall([mintSupplyCall]);
   });
 
   it('should be able to mint and supply using multicall', async () => {
-    let supplyAmount = e18('50');
-    let incLiquidityAmount = e18('20');
+    const supplyAmount = e18('50');
+    const incLiquidityAmount = e18('20');
 
-    const mintSupplyCall = await manager.connect(alice).interface.encodeFunctionData('mint', [
+    const mintSupplyCall = await manager.connect(ant).interface.encodeFunctionData('mint', [
       {
         asset: tokenA.target,
         pool: pool.target,
@@ -48,123 +59,58 @@ describe.only('NFT position manager - multicall', () => {
       },
     ]);
 
-    await manager.connect(alice).multicall([mintSupplyCall, liqiuidityIncreaseCall]);
+    await manager.connect(ant).multicall([mintSupplyCall, liqiuidityIncreaseCall]);
 
-    const balance = await manager.getPosition(1);
+    const balance = await manager.getPosition(2);
 
     // token A
     expect(balance.assets[0].balance).eq(supplyAmount);
-    expect(await tokenA.balanceOf(pool.target)).eq(supplyAmount);
+    expect(balance.assets[0].debt).eq(0);
+    expect(await tokenA.balanceOf(pool.target)).eq(supplyAmount + e18('100'));
 
     // token B
     expect(balance.assets[1].balance).eq(incLiquidityAmount);
+    expect(balance.assets[1].debt).eq(0);
     expect(await tokenB.balanceOf(pool.target)).eq(incLiquidityAmount);
   });
 
-  it.skip('should be able to repay and withdraw using multicall', async () => {
-    let mintAmount = e18('100');
-    let supplyAmount = e18('50');
-    let increaseLiquidityAmount = e18('20');
-    let borrowAmount = e18('20');
-    let repayAmount = e18('20');
-    let withdrawAmount = e18('70');
-    let multiCallData;
+  it('should be able to supply and borrow using multicall', async () => {
+    const supplyAmount = e18('50');
+    const borrowAmount = e18('20');
 
-    const mintParams = {
-      asset: tokenA.target,
-      pool: pool.target,
-      amount: supplyAmount,
-      data: { interestRateData: '0x', hookData: '0x' },
-    };
-
-    await tokenA.connect(alice)['mint(uint256)'](mintAmount);
-    await tokenA.connect(alice).approve(manager.target, supplyAmount);
-    const mintSupplyCall = await manager
-      .connect(alice)
-      .interface.encodeFunctionData('mint', [mintParams]);
-
-    // multiCallData = [mintSupplyCall];
-    // await manager.connect(alice).multicall(multiCallData);
-
-    let supplyBalanceAlice = await manager.getPosition(1);
-    expect(supplyBalanceAlice[0][0][1]).eq(supplyAmount);
-    // expect(await manager.ownerOf(1)).eq(await alice.getAddress());
-
-    const liquidityParams = {
-      asset: tokenA.target,
-      pool: pool.target,
-      amount: increaseLiquidityAmount,
-      tokenId: 1,
-      data: { interestRateData: '0x', hookData: '0x' },
-    };
-
-    await tokenA.connect(alice).approve(manager.target, increaseLiquidityAmount);
-
-    const liqiuidityIncreaseCall = manager.interface.encodeFunctionData('increaseLiquidity', [
-      liquidityParams,
+    // prepare multicall for the ant
+    const mintSupplyCallBob = await manager.interface.encodeFunctionData('mint', [
+      {
+        asset: tokenB.target,
+        pool: pool.target,
+        amount: supplyAmount,
+        data: { interestRateData: '0x', hookData: '0x' },
+      },
     ]);
+    const borrowCall = manager.interface.encodeFunctionData('borrow', [
+      {
+        asset: tokenA.target,
+        amount: borrowAmount,
+        tokenId: 2,
+        data: { interestRateData: '0x', hookData: '0x' },
+      },
+    ]);
+    await manager.connect(ant).multicall([mintSupplyCallBob, borrowCall]);
 
-    multiCallData = [mintSupplyCall, liqiuidityIncreaseCall];
-    await manager.connect(alice).multicall(multiCallData);
+    const balance = await manager.getPosition(2);
 
-    supplyBalanceAlice = await manager.getPosition(1);
-    expect(supplyBalanceAlice[0][0][1]).eq(supplyAmount + increaseLiquidityAmount);
-    expect(await tokenA.balanceOf(pool.target)).eq(supplyAmount + increaseLiquidityAmount);
+    // token A - borrowed
+    expect(balance.assets[0].debt).eq(borrowAmount);
+    expect(balance.assets[0].balance).eq(0);
+    expect(await tokenA.balanceOf(await ant.getAddress())).eq(borrowAmount + e18('100'));
 
-    const borrowParams = {
-      asset: await tokenA.getAddress(),
-      amount: borrowAmount,
-      tokenId: 1,
-      data: { interestRateData: '0x', hookData: '0x' },
-    };
+    // token B - supplied
+    expect(balance.assets[1].balance).eq(supplyAmount);
+    expect(balance.assets[1].debt).eq(0);
+    expect(await tokenB.balanceOf(pool.target)).eq(supplyAmount);
+  });
 
-    const borrowCall = manager.interface.encodeFunctionData('borrow', [borrowParams]);
-
-    multiCallData = [borrowCall];
-    await manager.connect(alice).multicall(multiCallData);
-
-    let borrowBalanceAlice = await manager.getPosition(1);
-    expect(borrowBalanceAlice[0][0][2]).eq(borrowAmount);
-    expect(await tokenA.balanceOf(pool.target)).eq(
-      supplyAmount + increaseLiquidityAmount - borrowAmount
-    );
-
-    let repayParams = {
-      asset: await tokenA.getAddress(),
-      amount: repayAmount,
-      tokenId: 1,
-      data: { interestRateData: '0x', hookData: '0x' },
-    };
-
-    await tokenA.connect(alice).approve(manager.target, repayAmount);
-
-    let repayCall = manager.interface.encodeFunctionData('repay', [repayParams]);
-    multiCallData = [repayCall];
-    await manager.connect(alice).multicall(multiCallData);
-
-    borrowBalanceAlice = await manager.getPosition(1);
-    expect(borrowBalanceAlice[0][0][2]).eq(0);
-
-    let withdrawParams = {
-      asset: await tokenA.getAddress(),
-      amount: withdrawAmount,
-      tokenId: 1,
-      data: { interestRateData: '0x', hookData: '0x' },
-    };
-
-    const withdrawCall = manager.interface.encodeFunctionData('withdraw', [withdrawParams]);
-    multiCallData = [withdrawCall];
-    await manager.connect(alice).multicall(multiCallData);
-
-    supplyBalanceAlice = await manager.getPosition(1);
-    expect(supplyBalanceAlice[0][0][1]).eq(0);
-    expect(await tokenA.balanceOf(await alice.getAddress())).eq(mintAmount);
-
-    const burnCall = manager.interface.encodeFunctionData('burn', ['1']);
-    multiCallData = [burnCall];
-
-    await manager.connect(alice).multicall(multiCallData);
-
-    expect(await manager.balanceOf(await alice.getAddress())).eq(0);
+  it.skip('should be able to repay and withdraw using multicall', async () => {
+    // todo
   });
 });
